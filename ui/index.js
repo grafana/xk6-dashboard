5b5ff852d7d8655b4974cc0b4c6127da8683e32d
Iván Szkiba <szkiba@users.noreply.github.com>
2021-06-10 20:52:15 +0200

    style: clean up UI code

--- ui/index.js
+++ ui/index.js
@@ -1,3 +1,14 @@
+import uPlot from "https://cdn.jsdelivr.net/npm/uplot@1.6.12/dist/uPlot.esm.min.js";
+
+if (document) {
+  document
+    .getElementsByTagName("head")[0]
+    .insertAdjacentHTML(
+      "beforeend",
+      '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uplot@1.6.12/dist/uPlot.min.css" />'
+    );
+}
+
 export const palette = ["#1b9e77", "#d95f02", "#7570b3", "#e7298a", "#66a61e", "#e6ab02", "#a6761d", "#666666"];
 
 export class SampleSource extends EventSource {
@@ -7,7 +18,26 @@ export class SampleSource extends EventSource {
     this.length = 0;
     this.time = [];
     this.sample = {};
+    this.metrics = {};
     this.addEventListener("message", (e) => this._onmessage(e));
+
+    // wait until all metrics metadata available
+    const api = url.replace("events/sample", "api/metrics");
+    this._peding = () => {
+      fetch(api)
+        .then((r) => r.json())
+        .then((v) => {
+          if (v.http_reqs) {
+            for (var key in v) {
+              const k = key + "_rate";
+              if (v[key].type == "counter" && !this.sample[k]) {
+                this.sample[k] = new Array(this.length);
+              }
+            }
+            delete this._peding;
+          }
+        });
+    };
   }
 
   view(...keys) {
@@ -35,6 +65,11 @@ export class SampleSource extends EventSource {
   }
 
   _onmessage(event) {
+    // check pending metrics metadata
+    if (this._peding) {
+      this._peding();
+    }
+
     const data = JSON.parse(event.data);
     const now = Math.floor(Date.now() / 1000);
 
@@ -42,6 +77,13 @@ export class SampleSource extends EventSource {
       if (!this.sample[key]) {
         this.sample[key] = new Array(this.length);
       }
+
+      const r = key + "_rate";
+      if (this.sample[r]) {
+        const last = this.length - 1;
+        this.sample[r].push(Math.abs(this.sample[key][last] - data[key]) / (now - this.time[last]));
+      }
+
       this.sample[key].push(data[key]);
     }
 
@@ -52,10 +94,47 @@ export class SampleSource extends EventSource {
       this.time.shift();
       for (const key of Object.keys(this.sample)) {
         this.sample[key].shift();
+        const r = key + "_rate";
+        if (this.sample[r]) {
+          this.sample[r].shift();
+        }
       }
       this.length--;
     }
 
     this.dispatchEvent(new Event("sample"));
+  }
+}
+
+export class SimpleChart {
+  constructor(src, { title, dst = "#charts" }, ...keys) {
+    this.dst = dst;
+    this.title = title;
+    this.src = src;
+    this.keys = keys;
+    src.addEventListener("sample", (e) => this.listener(e));
+  }
+
+  listener({ target }) {
+    const data = target.view(...this.keys);
+
+    if (!data) {
+      return;
+    }
+
+    if (this.chart) {
+      this.chart.setData(data);
+      return;
+    }
+
+    let opts = {
+      title: this.title,
+      class: "card",
+      width: window.innerWidth - 20,
+      height: 240,
+      series: data.series,
+    };
+
+    this.chart = new uPlot(opts, data, document.querySelector(this.dst));
   }
 }
