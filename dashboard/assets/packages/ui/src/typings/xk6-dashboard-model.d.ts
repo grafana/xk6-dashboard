118c8d0ebe6be834f4567e3ae681f5f1b4723e48
Iván SZKIBA <szkiba@users.noreply.github.com>
2023-10-10 17:43:53 +0200

    chore: add license headers

--- dashboard/assets/packages/ui/src/typings/xk6-dashboard-model.d.ts
+++ dashboard/assets/packages/ui/src/typings/xk6-dashboard-model.d.ts
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2023 Raintank, Inc. dba Grafana Labs
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
 import "@xk6-dashboard/model"
 
 import { DigestConfig } from "types/config"
