12a7803dac29a1d5bd02b261bb6e7fd0df98507d
Ben Simpson <ben.simpson@grafana.com>
2023-10-11 16:50:46 +0100

    [tools] Add bundle analyzer

--- dashboard/assets/packages/ui/vite.config.ts
+++ dashboard/assets/packages/ui/vite.config.ts
@@ -7,10 +7,18 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
 import svgr from "vite-plugin-svgr"
+import { visualizer } from "rollup-plugin-visualizer"
 import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
-  plugins: [svgr(), react(), tsconfigPaths()],
+  plugins: [
+    svgr(),
+    react(),
+    tsconfigPaths(),
+    visualizer({
+      filename: "bundle-stats.html"
+    })
+  ],
   build: { chunkSizeWarningLimit: 512 /*, outDir: '../../ui'*/ },
   base: ""
 })
