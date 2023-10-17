726dcff5e8b5e3c3d6073effef43199d0c85d01c
Ben Simpson <ben.simpson@grafana.com>
2023-10-17 14:44:19 +0100

    [fix] Remove node_modules

--- dashboard/assets/packages/ui/src/main.tsx
+++ dashboard/assets/packages/ui/src/main.tsx
@@ -1,8 +1,6 @@
-// SPDX-FileCopyrightText: 2023 Iván Szkiba
 // SPDX-FileCopyrightText: 2023 Raintank, Inc. dba Grafana Labs
 //
 // SPDX-License-Identifier: AGPL-3.0-only
-// SPDX-License-Identifier: MIT
 
 import React from "react"
 import ReactDOM from "react-dom/client"
