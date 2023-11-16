9c11f9fd3c7aa676570e33e1c7a189b56964019d
Ben Simpson <ben.simpson@grafana.com>
2023-11-16 12:58:08 +0000

    [refactor] Add docs for new utils

--- dashboard/assets/packages/ui/src/utils/object.ts
+++ dashboard/assets/packages/ui/src/utils/object.ts
@@ -9,6 +9,9 @@ export const omitUndefined = <T extends object>(styles: T) => {
   )
 }
 
+/**
+ * This method is used to pick the specified properties from the object
+ */
 export const pick = (props: string[], obj: object) =>
   Object.entries(obj).reduce(
     (acc, [key, value]) => {
@@ -21,6 +24,12 @@ export const pick = (props: string[], obj: object) =>
     {} as { [key: string]: unknown }
   )
 
+/**
+ * This method is used to merge two objects together. It will overwrite the values of the first object with the values
+ */
 export const mergeRight = (a: object, b: object) => ({ ...a, ...b })
 
+/**
+ * This method is used to merge two objects together. It will overwrite the values of the first object with the specified properties from the second object
+ */
 export const mergeRightProps = (props: string[]) => (a: object, b: object) => mergeRight(a, pick(props, b))
