7f897de414ac6a02b131653a213938c409f2d0ef
Iván SZKIBA <szkiba@users.noreply.github.com>
2024-01-12 17:09:13 +0100

    remove custom dashboard config

--- .dashboard.js
+++ .dashboard.js
@@ -3,10 +3,5 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 
 export default function (config) {
-  config.tabs.push({
-    title: "Custom",
-    id: "custom",
-    sections: [],
-  });
   return config;
 }
